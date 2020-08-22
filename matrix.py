22ba84ebb548687dbfdfedd2853fc0206e3805c7
Chalgorithm <67346639+Chalgorithm@users.noreply.github.com>
2020-08-22 16:57:17 -0700

    Add files via upload

--- matrix.py
+++ matrix.py
@@ -35,13 +35,18 @@ class Matrix:
             return Matrix(mat)
         elif type(right) == Matrix:
             mat = []
-            for y in range(0,right.columns):
-                mat.append([])
-                for x in range(0,self.rows):
-                    mat[y].append(0)
-            #multiplication goes here
-            
+            for y in range(0,right.rows):
+                mat.append([0]*len(self.matrix[0]))
+                
+            print(Matrix(mat))
+            print("rows:"+str(len(mat)))
+            print("cols:"+str(len(mat[0])))
+            #print("columns:"+str(len(mat[0])))
+            for y in range(0,len(mat)):
+                for x in range(0,len(mat)):
+                    mat[x][y] = self.dotProduct(right,y,x)
             return Matrix(mat)
+  
     def get(self,column,row):
         return self.matrix[column][row]
 
@@ -53,6 +58,7 @@ class Matrix:
         for y in range(0,self.rows):
             result.append(self.matrix[y][column])
         return result
+
     def dotProduct(self,othermat,column,row):
         sum = 0
         for x,y in zip(self.getColumn(column),othermat.getRow(row)):
